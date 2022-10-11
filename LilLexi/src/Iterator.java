a7d923ae748f1e428dc1437488aabb65f2345b62
KateN1996 <98198608+KateN1996@users.noreply.github.com>
2022-10-10 18:51:12 -0700

    Kate's Finalized Comments

--- LilLexi/src/Iterator.java
+++ LilLexi/src/Iterator.java
@@ -1,13 +1,38 @@
-
+/**
+ * 
+ * @author Kate Nixon
+ * 
+ * CSC 335 - David
+ * 
+ * Program: Iterator.java
+ * Overall Program: LilLexi.java
+ * 
+ * Abstract class of Iterator. Implemented by ArrayListIterator
+ * 
+ * Used to Iterate through any data structure
+ *
+ */
 public abstract class Iterator {
 	
+	/**
+	 * sets pointer to first object
+	 */
 	public abstract void First();
 	
+	/**
+	 * sets pointer to next object
+	 */
 	public abstract void Next();
 	
+	/**
+	 * returns boolean value if there are no
+	 * more objects to iterate through
+	 * @return
+	 */
 	public abstract boolean isDone();
 	
+	/**
+	 * dereferences the iterator to return the glyph it points to
+	 */
 	public abstract Glyph getCurrent();
-
-	public abstract void insert(Glyph g);
 }
