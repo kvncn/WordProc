5b0d568fc9301ef9c6fab2bfc57e4b9619fc788e
KateN1996 <98198608+KateN1996@users.noreply.github.com>
2022-10-14 21:27:07 -0700

    Fixed Wrap around - final version

--- LilLexi/src/Compositor.java
+++ LilLexi/src/Compositor.java
@@ -38,6 +38,9 @@ public class Compositor {
 		int y = -1;
 		int maxWidth = 700;
 		int rowWidth = 0;
+		int curY = 0;
+
+		int prevHeight = 0;
 		column = new ArrayList<Glyph>();
 		for (Glyph g : glyphs) {
 			rowWidth += g.bounds().width;
@@ -45,19 +48,27 @@ public class Compositor {
 				x = 0;
 				rowWidth = g.bounds().width;
 				y++;
-				Glyph newRow = new Row(x, y);
+				if(column.size() > 0) {
+					prevHeight += column.get(column.size()-1).maxHeight;
+				}
 				
+				Glyph newRow = new Row(x, y, prevHeight);
 				g.setY(y);
 				newRow.insert(g);
+				
+				newRow.setCurY(curY);
 				column.add(newRow);
 			} else {
 				g.setY(y);
 				Row curRow = (Row) column.get(column.size()-1);
 				curRow.insert(g);
 				x++;
+				
+				curRow.setCurY(curY);
 			}
 		}	
 		return column;
 	}
 }
+
 
